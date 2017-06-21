5a9aed4bfb0a6ed36d9e42275695c89a2ce7605f
Dihara Wijetunga <shredhead94@gmail.com>
2017-06-22 02:13:52 +0530

    Updated windows build scripts

--- build_release_x64.bat
+++ build_release_x64.bat
@@ -1,2 +1,2 @@
-cd external/PhysX/PhysX_3.4/Source/compiler/vc14win64
-msbuild.exe PhysX.sln /t:Build /p:OutDir="..\..\..\..\..\..\lib\release\PhysX" /p:Configuration="release"
+@ Build PhysX
+msbuild.exe "external/PhysX/PhysX_3.4/Source/compiler/vc14win64/PhysX.sln" /t:Build /p:OutDir="..\..\..\..\..\..\lib\release\PhysX" /p:Configuration="release"
