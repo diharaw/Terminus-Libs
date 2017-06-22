a74a324400b4e0dc385c07069dca62fa4a511167
Dihara Wijetunga <dihara.w@fidenz.com>
2017-06-22 17:12:33 +0530

    Added partial macOS build script

--- build_macos_x64_debug.sh
+++ build_macos_x64_debug.sh
@@ -1,2 +1,56 @@
+# ------------------------------------------------
+# Build PhysX
+# ------------------------------------------------
 
-xcodebuild -project Terminus-Engine.xcworkspace -scheme TerminusRuntime CONFIGURATION_BUILD_DIR=test_bin  build
+#xcodebuild -project "external/PhysX/PhysX_3.4/Source/compiler/xcode_osx64/PhysX.xcodeproj" CONFIGURATION_BUILD_DIR="../../../../../../lib/debug/PhysX"  -configuration debug build
+
+# ------------------------------------------------
+# Build CMake Projects
+# ------------------------------------------------
+
+mkdir -p projects
+rm -r projects/*
+cd projects
+
+# ------------------------------------------------
+# Build SDL
+# ------------------------------------------------
+mkdir -p SDL
+cd SDL
+cmake -DSDL_SHARED=OFF -DSDL_STATIC=ON -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../../external/SDL" -G "Xcode"
+cd ..
+cd ..
+
+xcodebuild -project "projects/SDL/SDL2.xcodeproj" CONFIGURATION_BUILD_DIR="../../lib/debug/SDL"  -configuration Debug build
+
+# ------------------------------------------------
+# Build glslang
+# ------------------------------------------------
+cd projects
+mkdir -p glslang
+cd glslang
+cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../../external/glslang" -G "Xcode"
+cd ..
+cd ..
+
+xcodebuild -project "projects/glslang/glslang.xcodeproj" CONFIGURATION_BUILD_DIR="../../lib/debug/glslang"  -configuration Debug build
+
+# ------------------------------------------------
+# Build assimp
+# ------------------------------------------------
+
+# ------------------------------------------------
+# Build nvidia-texture-tools
+# ------------------------------------------------
+cd projects
+mkdir -p nvidia-texture-tools
+cd nvidia-texture-tools
+cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../../external/nvidia-texture-tools" -G "Xcode"
+cd ..
+cd ..
+
+xcodebuild -project "projects/nvidia-texture-tools/NV.xcodeproj" CONFIGURATION_BUILD_DIR="../../lib/debug/nvidia-texture-tools"  -configuration Debug build
+
+# ------------------------------------------------
+# Build luajit
+# ------------------------------------------------
--- build_macos_x64_release.sh
+++ build_macos_x64_release.sh
@@ -1,2 +1,47 @@
+# ------------------------------------------------
+# Build PhysX
+# ------------------------------------------------
 
-xcodebuild -workspace Terminus-Engine.xcworkspace -scheme TerminusRuntime CONFIGURATION_BUILD_DIR=test_bin  build
+xcodebuild -project "external/PhysX/PhysX_3.4/Source/compiler/xcode_osx64/PhysX.xcodeproj" CONFIGURATION_BUILD_DIR="../../../../../../lib/release/PhysX"  -configuration release build
+
+# ------------------------------------------------
+# Build CMake Projects
+# ------------------------------------------------
+
+mkdir -p projects
+rm -r projects/*
+cd projects
+
+# ------------------------------------------------
+# Build SDL
+# ------------------------------------------------
+mkdir -p SDL
+cd SDL
+cmake -DSDL_SHARED=OFF -DSDL_STATIC=ON -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../../external/SDL" -G "Xcode"
+cd ..
+cd ..
+
+xcodebuild -project "projects/SDL/SDL2.xcodeproj" CONFIGURATION_BUILD_DIR="../../lib/release/SDL"  -configuration Release build
+
+# ------------------------------------------------
+# Build glslang
+# ------------------------------------------------
+
+# ------------------------------------------------
+# Build assimp
+# ------------------------------------------------
+
+# ------------------------------------------------
+# Build nvidia-texture-tools
+# ------------------------------------------------
+mkdir -p nvidia-texture-tools
+cd nvidia-texture-tools
+cmake -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ "../../external/nvidia-texture-tools" -G "Xcode"
+cd ..
+cd ..
+
+xcodebuild -project "projects/nvidia-texture-tools/NV.xcodeproj" CONFIGURATION_BUILD_DIR="../../lib/release/nvidia-texture-tools"  -configuration Release build
+
+# ------------------------------------------------
+# Build luajit
+# ------------------------------------------------
