dac12c5b2be7c0c268c507332361e84b08768b30
Dihara Wijetunga <dihara.w@fidenz.com>
2017-06-22 11:57:55 +0530

    Added empty build scripts for macOS, iOS and Android

---
+++ build_android_armv8_debug.sh
@@ -0,0 +1,2 @@
+
+xcodebuild -workspace Terminus-Engine.xcworkspace -scheme TerminusRuntime CONFIGURATION_BUILD_DIR=test_bin  build
---
+++ build_macos_x64_debug.sh
@@ -0,0 +1,2 @@
+
+xcodebuild -project Terminus-Engine.xcworkspace -scheme TerminusRuntime CONFIGURATION_BUILD_DIR=test_bin  build
